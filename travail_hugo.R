d08c8fa496f2a3e13414c8835fa8b9b8d742c032
BrouckeHugo <brouckeh@gmail.com>
2019-10-27 17:13:27 +0100

    edit stat des, créer présentation: à modif avec code camille + présentation

--- travail_hugo.R
+++ travail_hugo.R
@@ -1,82 +1,35 @@
-rm(list=ls()) #remove everything that is in the environment
+#Mettre le year format date
+data_nat_clean$year <- str_pad(data_nat_clean$year,5,"right")
+data_nat_clean$year <- str_replace(data_nat_clean$year," ","-31-12")
+data_nat_clean$year <- as.Date(data_nat_clean$year, format="%Y-%d-%m")
 
-#Importe les données d'un site et on donne un nom à notre dataframe
-library(readr)
-library(tidyverse)
-library(stats)
-getwd()
-data <- read.csv("nat2018.csv",sep = ";", encoding = "UTF-8")
+#Calculer le nombre de prénom unique
+distinct_names <- data_nat_clean %>% 
+                  group_by(year) %>% 
+                  distinct(firstname) %>%
+                  summarise(n_names = n())
 
-data2 <- data %>% filter(!is.na(preusuel)) %>% #Remove empty firstname
-  filter(preusuel != "_PRENOMS_RARES") %>% #Remove rare firstnames
-  filter(annais != "XXXX") %>% #Remove year 2019
-  rename(sex = sexe, year = annais, firstname = preusuel, number = nombre) #Rename variables to english
+#Plot le résultat                  
+plot_distinct_names <- 
+  distinct_names %>% 
+  ggplot(aes(x=year, y=n_names)) + 
+  geom_line(size=1.2, color="blue") +         
+  ggtitle("Number of French unique names from 1900 to 2018") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Year") + ylab("Number of unique names")
 
-# Statistiques de bases
-n <- data2 %>% select(firstname) %>% distinct() %>% nrow() 
-ngirls <- data2 %>% filter(sex==2) %>% select(firstname) %>% distinct() %>% nrow() 
-nboys <- data2 %>% filter(sex==1) %>% select(firstname) %>% distinct() %>% nrow()
+plot_distinct_names
 
-# Graphique des prénoms différents par années
+#Ajoute la différence selon l'année
+distinct_names$ratio <- ((distinct_names$n_names-lag(distinct_names$n_names))/
+                           (distinct_names$n_names+lag(distinct_names$n_names))*100)
 
-
-
-rm(list=ls()) #remove everything that is in the environment
-
-#Importe les données d'un site et on donne un nom à notre dataframe
-library(readr)
-library(tidyverse)
-library(stats)
-getwd()
-data <- read.csv("nat2018.csv",sep = ";", encoding = "UTF-8")
-
-
-#mettre la date au format année
-test <- data %>% filter(preusuel == "ZINEDINE")
-test$annais <- str_replace(test$annais,"XXXX","2019-15-07")
-test$annais <- str_pad(test$annais,5,"right")
-test$annais <- str_replace(test$annais," ","-31-12")
-test$annais <- as.Date(test$annais, format="%Y-%d-%m")
-
-
-#obtenir le lag
-test$lag <- lag(test$nombre)
-#obtenir la diff d'une aannée à l'autre
-test <- test %>% mutate(diff = nombre-lag)
-
-ggplot(test)+geom_line(aes(x=annais, y=nombre), col="black")+geom_line(aes(x=annais, y =diff), col="red")  
-
-#Nombre de prénom différents existant
-n <- data %>% select(preusuel) %>% distinct() %>% nrow() 
-n
-
-
-
-#####
-data2 <- data %>% filter(!is.na(preusuel)) %>% #Remove empty firstname
-  filter(preusuel != "_PRENOMS_RARES") %>% #Remove rare firstnames
-  filter(annais != "XXXX") %>% #Remove year 2019
-  rename(sex = sexe, year = annais, firstname = preusuel, number = nombre) #Rename variables to english
-
-#Transform factor variables to characters
-data2$firstname <- as.character(data2$firstname)
-data2$year <- as.character(data2$year)
-
-data2$lag <- 0
-nr <- nrow(data2)
-
-for(i in 1:nr) {
-  if (i == data2$sex){
-    data2[i,data2$lag] <- data2[i,data2$number]
-  } else {
-    if (data2[i,data2$firstname] == data2[i-1,data2$firstname]){
-      data2[i,data2$lag] <- data2[i-1,data2$number]
-    } else {
-      data2[i,data2$lag] <- data2[i,data2$number]
-    }
-  }
-  print(i)
-}  
-
-data$diff <- nom-lag
-
+#Plot la différence d'augmentation
+plot_ratio <- distinct_names %>% 
+  ggplot(aes(x=year, y=ratio)) + 
+  geom_hline(yintercept=0, size = 1) +
+  geom_line(size = 1.2, color = "red") +
+  ggtitle("Evolution of names from 1900 to 2018") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Year") + ylab("Percentage of difference")
+plot_ratio
