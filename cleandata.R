1a1b7ecc2e1c52e141fb8a2c34010078d1bebb6e
BrouckeHugo <brouckeh@gmail.com>
2019-10-27 13:06:39 +0100

    propre

--- cleandata.R
+++ cleandata.R
@@ -10,7 +10,7 @@ data_dpt <- read.csv("dpt2018.csv",sep = ";", encoding = "UTF-8")
 data_nat_clean <- data_nat %>% filter(!is.na(preusuel)) %>% #Remove empty firstname
   filter(preusuel != "_PRENOMS_RARES") %>% #Remove rare firstnames
   filter(annais != "XXXX") %>% #Remove year 2019
-  rename(sex = sexe, year = annais, firstname = preusuel, number = nombre) #Rename variables to english
+  rename(sex = X.U.FEFF.sexe, year = annais, firstname = preusuel, number = nombre) #Rename variables to english
 
 data_dpt_clean <- data_dpt %>% filter(!is.na(preusuel)) %>% #Remove empty firstname
   filter(preusuel != "_PRENOMS_RARES") %>% #Remove rare firstnames
